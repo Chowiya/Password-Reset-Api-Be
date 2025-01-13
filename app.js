22103156d488f8d79b817915b4bb5a7307d64444
Chowiya <nd1553410@gmail.com>
2025-01-13 19:24:06 +0530

    cores fixes

--- app.js
+++ app.js
@@ -9,10 +9,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: 'https://myapp-fe.netlify.app/',
+
+
+// Allow requests from both Netlify and local development
+const allowedOrigins = [
+    'https://myapp-fe.netlify.app',
+    'http://localhost:5173'  // For local testing
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true
-}))
+  }));
+
+  
 
 app.use('/auth',authRouter)
 
