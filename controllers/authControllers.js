5d19eacf390deac67931a70bc42e6004acf3fcdb
Chowiya <nd1553410@gmail.com>
2025-01-15 15:15:14 +0530

    updated

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -111,7 +111,7 @@ const authController = {
             from: process.env.USER_EMAIL,
             to: email,
             subject: 'Password Reset request',
-            text: `https://myapp-fe.netlify.app/auth/new-password/${token}`
+            text: `https://myapp-fe.netlify.app/new-password/${token}`
           };
           
           transporter.sendMail(mailOptions, function(error, info){
