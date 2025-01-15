749038bf960cb606606b0e1946e02ffddfd7f7f1
Chowiya <nd1553410@gmail.com>
2025-01-15 10:36:34 +0530

    Updated API URL

--- app.js
+++ app.js
@@ -10,23 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
-// Allow requests from both Netlify and local development
-const allowedOrigins = [
-    'https://myapp-fe.netlify.app',
-    'http://localhost:5173'  // For local testing
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+app.use(cors({
+    origin: 'https://myapp-fe.netlify.app',
     credentials: true
-  }));
+}))
+
+
 
   
 
